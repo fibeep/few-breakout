df18b2c64fdb238097505d56e5ad7b64cacd256e
Salo <salomonco@gmail.com>
2022-10-21 18:45:46 -0500

    fixed undefined bricks issue

--- Bricks.js
+++ Bricks.js
@@ -33,7 +33,7 @@ class Bricks {
     for (let c = 0; c < this.cols; c += 1) {
       for (let r = 0; r < this.rows; r += 1) {
         const brick = this.bricks[c][r];
-        if (bricks.status === 1) {
+        if (this.bricks.status === 1) {
           brick.render(ctx);
         }
       }
